6f14e9a6feb962a006b9329373704215f7ae360b
Carmen Luna <carmen.oprea@hotmail.com>
2019-03-27 10:41:36 +0100

    created uploads table

--- app/File.php
+++ app/File.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class File extends Model
 {
-    //
+    protected $fillable = ['subject', 'year', 'level'];
+
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
